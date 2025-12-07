7ab3fa47833be36d6013999b1ae8c2b1abeafb1f
thongster <106802301+thongster@users.noreply.github.com>
2025-12-07 17:55:42 +0700

    normalize and global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Grid from "./components/Grid.jsx"
 import Score from "./components/Score.jsx"
+import './styles/modern-normalize.css'
 import './styles/App.css'
 
 function App() {
