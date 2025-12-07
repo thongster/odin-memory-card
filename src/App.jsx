f653a73836fa2b3c0b054948af34c28b7b91cf42
thongster <106802301+thongster@users.noreply.github.com>
2025-12-07 17:53:44 +0700

    basic score component set up, minor global styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Grid from "./components/Grid.jsx"
+import Score from "./components/Score.jsx"
 import './styles/App.css'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
 
       <div className='container'>
+        <Score/> 
         <Grid />
       </div>
 
