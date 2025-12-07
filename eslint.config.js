675d68c66b60efe21bbb00c7c06b9c5be342f5f6
thongster <106802301+thongster@users.noreply.github.com>
2025-12-07 16:35:00 +0700

    react, eslint, prettier

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import prettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,jsx}'],
+    ignores: ['dist/**'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      prettier,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.flat.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+
+      // Prettier integration
+      'prettier/prettier': 'error',
+
+      // Disable prop-types requirement (TOP doesn’t use PropTypes)
+      'react/prop-types': 'off',
+    },
+  },
+
+  // Disable ESLint rules that conflict with Prettier
+  eslintConfigPrettier,
+])
