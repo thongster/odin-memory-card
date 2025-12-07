79050a8649641ffb474756b3de9b7c9ad65d3e94
thongster <106802301+thongster@users.noreply.github.com>
2025-12-08 01:32:56 +0700

    clean up function for fetch

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,25 +2,30 @@ import { useState, useEffect } from 'react'
 import "../styles/Card.css"
 
 export default function Card() {
-    const [char, setChar] = useState("");
+    const [char, setChar] = useState(null);
 
     useEffect(() => {
+        const controller = new AbortController();
+        const signal = controller.signal
+
         const fetchData = async () => {
             try {
-                const response = await fetch('https://dragonball-api.com/api/characters/')
+                const response = await fetch('https://dragonball-api.com/api/characters/', {signal})
                 const data = await response.json()
-                setChar(data)
-                
+                setChar(data.items)
+                console.log(data.items)
             } catch {
                 console.log("error")
             }
         }
         fetchData()
+
+        return () => {controller.abort()}
     }, [])
 
     return (
         <div className="card">
-            
+            {char && char[0] && char[0].name}
         </div>
     )
 }
