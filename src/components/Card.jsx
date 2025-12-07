064763a3e011e0692abeaa9e4b55b2b59ad4abae
thongster <106802301+thongster@users.noreply.github.com>
2025-12-08 01:18:40 +0700

    start async await for dbz api

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import "../styles/Card.css"
 
 export default function Card() {
+    const [char, setChar] = useState("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://dragonball-api.com/api/characters/')
+                const data = await response.json()
+                setChar(data)
+                
+            } catch {
+                console.log("error")
+            }
+        }
+        fetchData()
+    }, [])
 
     return (
         <div className="card">
