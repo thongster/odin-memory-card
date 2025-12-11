20905d608b9084c93c0a5544d1728f4533a275a6
thongster <106802301+thongster@users.noreply.github.com>
2025-12-11 12:50:06 +0700

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import "../styles/Header.css"
+
+export default function Header() {
+    return (
+        <header className="header">
+        <h1>Dragon Ball Memory Game</h1>
+        </header>
+    )
+}
